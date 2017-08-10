744b302a28ff75a41b314c775efde9a17999034e
Simon Iversen <simo002m@gmail.com>
2017-08-10 21:13:28 +0200

    Added a setup

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#Type su before running this
+mkdir ~/repos
+
+#'Homemade' repos
+mkdir ~/repos/gcryptor_repo
+mkdir ~/repos/cryptor_repo
+mkdir ~/repos/pcal_repo
+mkdir ~/repos/configs
+mkdir ~/repos/mfc_repo
+
+mkdir ~/repos/
+
+git clone https://github.com/simo002m/GCryptor.git ~/repos/gcryptor_repo
+git clone https://github.com/simo002m/Cryptor.git ~/repos/cryptor_repo
+git clone https://github.com/simo002m/PCal.git ~/repos/pcal_repo
+git clone https://github.com/simo002m/configs.git ~/repos/configs
+git clone https://github.com/simo002m/mfc.git ~/repos/mfc_repo
+
+git clone https://github.com/LinxGem33/Plank-Themes.git ~/
+
+apt-get install -y vim
+apt-get install -y tmux 
+apt-get install -y python3-tk
+apt-get install -y mate-terminal
+apt-get install -y plank
+apt-get install -y dmenu
+apt-get install -y virtualbox 
+
+apt-add-repository ppa:noobslab/macbuntu
+apt-get update
+
+sudo apt-get install macbuntu-os-icons-lts-v7
+sudo apt-get install macbuntu-os-ithemes-lts-v7
+sudo apt-get install macbuntu-os-plank-theme-lts-v7
+
+apt-get install -y links2
+apt-get install -y nsnake
+apt-get install -y arduino
+apt-get install -y idle3
+apt-get install -y emacs
+apt-get install -y leafpad
+apt-get install -y ipython3
+
