f02517206710e460960e515c2b8eb5ceb5e9bfd7
Simon Iversen <simo002m@gmail.com>
2017-09-09 13:59:19 +0200

    Now it asks for install texlive-full or not, due to it being quite a big package

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from subprocess import call
 
+confirm_latex = input("texlive-full is quite a big package, do you want to install it or not? (yes/no) ")
+
 call(["mkdir", "~/repos"])
 
 # directories for 'homemade' repos
@@ -28,4 +30,9 @@ call(["apt-get", "update"])
 program_list = ["vim", "tmux", "python3-tk", "python3-pip", "plank", "mate-terminal", "albert", "openssh-server", "virtualbox", "dmenu", "macbuntu-os-icons-lts-v7", "macbuntu-os-ithemes-lts-v7", "macbuntu-os-plank-theme-lts-v7", "w3m", "w3m-img", "links2", "nsnake", "idle3", "emacs", "leafpad", "ipython3", "gcolor2"]
 
 for program in program_list:
-	call(["sudo", "apt", "install", "-y", program])
+        call(["sudo", "apt", "install", "-y", program])
+
+if confirm_latex == "yes" or confirm_latex == "Yes" or confirm_latex == "YES" or confirm_latex == "y" or confirm_latex == "Y":
+    call(["sudo", "apt", "install", "-y", "texlive-full"])
+else:
+    pass
