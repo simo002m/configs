c22d0c9f06d4d36d4ea5d192994269bdba56a3eb
Simon Iversen <simo002m@gmail.com>
2017-09-09 13:52:25 +0200

    Made the setup script more effective

--- setup.py
+++ setup.py
@@ -2,25 +2,30 @@ from subprocess import call
 
 call(["mkdir", "~/repos"])
 
-#'Homemade' repos
-call(["mkdir", "~/repos/gcryptor_repo"])
-call(["mkdir", "~/repos/cryptor_repo"])
-call(["mkdir", "~/repos/mfc_repo"])
-call(["mkdir", "~/repos/configs"])
+# directories for 'homemade' repos
+dirs = ["gcryptor", "cryptor", "mfc", "mfc_cli", "configs"]
 
+for directory in dirs: 
+    call(["mkdir", "~/repos/", directory])
 
-git clone https://github.com/simo002m/GCryptor.git ~/repos/gcryptor_repo
-git clone https://github.com/simo002m/Cryptor.git ~/repos/cryptor_repo
-git clone https://github.com/simo002m/PCal.git ~/repos/pcal_repo
-git clone https://github.com/simo002m/configs.git ~/repos/configs
-git clone https://github.com/simo002m/mfc.git ~/repos/mfc_repo
+repos = {
+        "gcryptor":["https://github.com/simo002m/GCryptor.git", "~/repos/gcryptor"],
+        "cryptor":["https://github.com/simo002m/Cryptor.git", "~/repos/cryptor"],
+        "mfc_cli":["https://github.com/simo002m/mfc_cli.git", "~/repos/mfc_cli"],
+        "mfc":["https://github.com/simo002m/mfc.git", "~/repos/mfc"],
+        "configs":["https://github.com/simo002m/configs.git", "~/repos/configs"],
 
-git clone https://github.com/LinxGem33/Plank-Themes.git ~/
+        "plank_themes":["https://github.com/LinxGem33/Plank-Themes.git" "~/"]
+        }
+
+for repo in repos:
+    call(["git", "clone", repo])
+
 
 call(["apt-add-repository", "ppa:noobslab/macbuntu"])
 call(["apt-get", "update"])
 
-program_list = [vim, tmux, python3-tk, python3-pip, plank, mate-terminal, albert, plank, openssh-server, virtualbox, dmenu, macbuntu-os-icons-lts-v7, macbuntu-os-ithemes-lts-v7, macbuntu-os-plank-theme-lts-v7, w3m, w3m-img, links2, nsnake, idle3, emacs, leafpad, ipython3, gcolor2]
+program_list = ["vim", "tmux", "python3-tk", "python3-pip", "plank", "mate-terminal", "albert", "openssh-server", "virtualbox", "dmenu", "macbuntu-os-icons-lts-v7", "macbuntu-os-ithemes-lts-v7", "macbuntu-os-plank-theme-lts-v7", "w3m", "w3m-img", "links2", "nsnake", "idle3", "emacs", "leafpad", "ipython3", "gcolor2"]
 
 for program in program_list:
 	call(["sudo", "apt", "install", "-y", program])
