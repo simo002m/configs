6556c3e32c1db12682f6fa7f7c03eb160fbaf1ef
Simon Iversen <simo002m@gmail.com>
2017-09-26 21:35:39 +0200

    Added a config updating script

---
+++ update_configs.sh
@@ -0,0 +1,15 @@
+rm configs/i3config
+rm ~/configs/polybar_config
+rm ~/configs/Xresources
+rm ~/configs/vimrc
+rm ~/configs/bashrc
+rm ~/configs/tmux.conf
+rm ~/configs/setup.py
+
+cp ~/.config/i3/config ~/configs/i3config
+cp ~/.config/polybar/config ~/config/polybar_config
+cp ~/.Xresources ~/configs/Xresources
+cp ~/configs/.vimrc ~/.vimrc
+cp ~/configs/.bashrc ~/.bashrc
+cp ~/configs/.tmux.conf ~/.tmux.conf
+cp ~/configs/.setup.py
